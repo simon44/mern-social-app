7d034276e1bbf4ea5c299f44cf0969dd16ed0f7c
Simon <simon44360@gmail.com>
2020-03-20 11:33:02 +0100

    Create profile component & action

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from '../actions/types';
 
 const initialState = {
   profile: null,
@@ -25,7 +25,13 @@ export default function(state = initialState, action) {
         loading: false,
         profile: null
       };
-
+    case CLEAR_PROFILE:
+      return {
+        ...state,
+        profile: null,
+        repos: [],
+        loading: false
+      };
     default:
       return state;
   }
