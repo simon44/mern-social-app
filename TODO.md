0a5664c3da4575660098cb8b9d0cc858f1116fba
Simon <simon44360@gmail.com>
2020-03-24 15:48:10 +0100

    Add and delete comment

--- TODO.md
+++ TODO.md
@@ -3,6 +3,8 @@ TODOS :
 - Fix npm security warnings
 - Add secrets to github using git-crypt
 - Add package size management (back and client)
-- Add step on how to localy run the project and how to deploy
 - Make import static ('src/actions/...' instead of '../../actions/...')
-- Add unit tests
+- Add unit tests :
+  - front
+  - back
+- Add a README file with info about how to build / heroku link / vscode plugins used etc.
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -4,7 +4,9 @@ import {
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
-  ADD_POST
+  ADD_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT
 } from '../actions/types';
 
 const initialState = {
@@ -53,6 +55,26 @@ export default function(state = initialState, action) {
       return {
         ...state,
         posts: state.posts.filter(post => post._id !== payload),
+        loading: false
+      };
+    case ADD_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          comments: payload
+        },
+        loading: false
+      };
+    case REMOVE_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          comments: state.post.comments.filter(
+            comment => comment._id !== payload
+          )
+        },
         loading: false
       };
     default:
